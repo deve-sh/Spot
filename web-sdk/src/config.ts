757e5e1bcd22cca5125acdb91f20ba8fb8ad7e07
Devesh Kumar <devesh2027@gmail.com>
2022-11-01 13:07:03 +0530

    Added data logging and monitoring endpoint

--- web-sdk/src/config.ts
+++ web-sdk/src/config.ts
@@ -3,6 +3,8 @@ const config = {
 		process.env.NODE_ENV !== 'development'
 			? 'https://spot-api.vercel.app'
 			: 'http://localhost:5432',
+	MONITORING_DATA_ENDPOINT: 'web-monitoring',
+	LOG_DATA_ENDPOINT: 'web-logs',
 	MONITORING_INTERVAL: 4000,
 	LOG_SENDING_DEBOUNCE_TIME: 1000
 };
---
+++ web-sdk/src/monitoring/getMonitoringEndpoint.ts
@@ -0,0 +1,11 @@
+import config from '../config';
+import { getInstance } from '../utils/instance';
+
+const getMonitoringEndpoint = () => {
+	const instance = getInstance();
+	if (!instance || !instance.projectId) return null;
+
+	return `${config.BACKEND_URL}/${instance.projectId}/${config.MONITORING_DATA_ENDPOINT}`;
+};
+
+export default getMonitoringEndpoint;
