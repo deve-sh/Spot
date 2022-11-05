e34f56889b2c71d2a9417274dc53fab839ee1fcd
Devesh Kumar <devesh2027@gmail.com>
2022-11-05 17:17:43 +0530

    frontend: Added explainer text for Web Vitals

--- frontend/src/components/Session/SessionVitals/Vital.tsx
+++ frontend/src/components/Session/SessionVitals/Vital.tsx
@@ -1,4 +1,5 @@
-import { Stat, StatHelpText, StatLabel, StatNumber } from '@chakra-ui/react';
+import React, { useMemo } from 'react';
+import { Stat, StatLabel, StatNumber, Tooltip } from '@chakra-ui/react';
 
 interface VitalProps {
 	vitalName: string;
@@ -9,6 +10,8 @@ const vitalsNameMapper = (name: string) => {
 	if (name === 'domComplete') return 'domComplete';
 	if (name === 'domContentLoadedEventEnd') return 'domContentLoadedEventEnd';
 	if (name === 'fcp') return 'First Contentful Paint';
+	if (name === 'fp') return 'First Paint';
+	if (name === 'fid') return 'First Input Delay';
 	if (name === 'loadEventEnd') return 'Load Event Complete';
 	if (name === 'domInteractive') return 'domInteractive';
 	if (name === 'transferSize') return 'Transfer Size';
@@ -19,15 +22,37 @@ const vitalsValueMapper = (name: string, value: number) => {
 	if (name === 'domContentLoadedEventEnd') return Number(value / 1000).toFixed(2) + 's';
 	if (name === 'domInteractive') return Number(value / 1000).toFixed(2) + 's';
 	if (name === 'fcp') return Number(value / 1000).toFixed(2) + 's';
+	if (name === 'fid') return Number(value / 1000).toFixed(2) + 's';
+	if (name === 'fp') return Number(value / 1000).toFixed(2) + 's';
 	if (name === 'loadEventEnd') return Number(value / 1000).toFixed(2) + 's';
 	if (name === 'transferSize') return Number(value / 1000).toFixed(2) + ' MB';
 };
 
-const Vital = (props: VitalProps) => (
-	<Stat minWidth="25%" borderRadius="lg" borderWidth="1.25px" borderColor="gray.200" padding="4">
-		<StatLabel>{vitalsNameMapper(props.vitalName)}</StatLabel>
-		<StatNumber>{vitalsValueMapper(props.vitalName, props.value)}</StatNumber>
-	</Stat>
-);
+const vitalExplainer = (name: string) => {
+	if (name === 'domComplete')
+		return 'Measures the time to fully build the DOM and execute any scripts in it.';
+	if (name === 'fcp')
+		return "Measures the time from when the page starts loading to when any part of the page's content is rendered on the screen.";
+	if (name === 'fid') return 'Measures the time from when a user first interacts with a page';
+};
+
+const Vital = (props: VitalProps) => {
+	const vitalsExplainingText = useMemo(() => vitalExplainer(props.vitalName), [props.vitalName]);
+	const Parent = vitalsExplainingText ? Tooltip : React.Fragment;
+	return (
+		<Parent label={vitalsExplainingText}>
+			<Stat
+				minWidth="25%"
+				borderRadius="lg"
+				borderWidth="1.25px"
+				borderColor="gray.200"
+				padding="4"
+			>
+				<StatLabel>{vitalsNameMapper(props.vitalName)}</StatLabel>
+				<StatNumber>{vitalsValueMapper(props.vitalName, props.value)}</StatNumber>
+			</Stat>
+		</Parent>
+	);
+};
 
 export default Vital;
