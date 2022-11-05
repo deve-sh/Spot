6e22f8bfe7eceb992d65bf38438c176ec691ae4f
Devesh Kumar <devesh2027@gmail.com>
2022-11-05 11:59:13 +0530

    Frontend: Better useFetch

--- frontend/src/hooks/useFetch.ts
+++ frontend/src/hooks/useFetch.ts
@@ -3,16 +3,37 @@ import useSWR from 'swr';
 import fetcher from '../utils/fetch';
 import useProjectAuth from './useProjectAuth';
 
-const useFetch = (key: string | null | undefined) => {
+interface UseFetchOptions {
+	headers?: Record<string, any>;
+	revalidateOnMount?: boolean;
+	revalidateOnFocus?: boolean;
+	revalidateIfStale?: boolean;
+	shouldRetryOnError?: boolean;
+	dedupingInterval?: number;
+	errorRetryCount?: number;
+	fallback?: any;
+}
+
+const useFetch = (key: string | null | undefined, options: UseFetchOptions = {}) => {
 	const projectAuth = useProjectAuth();
 
 	const fetch = useCallback(
 		(key: string): any =>
-			fetcher(key, { headers: { authorization: `Key ${projectAuth.apiKey}` } }),
+			fetcher(key, {
+				headers: options.headers || { authorization: `Key ${projectAuth.apiKey}` }
+			}),
 		[key, projectAuth?.apiKey]
 	);
 
-	return useSWR(key, fetch, { revalidateOnMount: true, revalidateOnFocus: false });
+	return useSWR(key, fetch, {
+		revalidateOnMount: options.revalidateOnMount || true,
+		revalidateOnFocus: options.revalidateOnFocus || false,
+		revalidateIfStale: options.revalidateIfStale || true,
+		fallback: options.fallback,
+		dedupingInterval: options.dedupingInterval || 2000,
+		shouldRetryOnError: options.shouldRetryOnError || false,
+		errorRetryCount: options.errorRetryCount || 0
+	});
 };
 
 export default useFetch;
