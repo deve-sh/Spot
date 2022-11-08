f8ebb1172a52a5178ed3e05a4285bec65647ce87
Devesh Kumar <devesh2027@gmail.com>
2022-11-09 00:00:53 +0530

    frontend: Added redirect for authorized user to projects page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,16 @@
-const HomePage = () => <></>;
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import useUser from 'hooks/auth/useUser';
+
+const HomePage = () => {
+	const router = useRouter();
+	const user = useUser();
+
+	useEffect(() => {
+		if (user) router.push('/projects');
+	}, [user]);
+
+	return <></>;
+};
 
 export default HomePage;
