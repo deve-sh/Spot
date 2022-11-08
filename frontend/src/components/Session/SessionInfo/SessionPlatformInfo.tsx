8e5cda0670652326099c60dd7206f8e4fbeeec01
Devesh Kumar <devesh2027@gmail.com>
2022-11-08 17:38:17 +0530

    frontend: Added tooltip to Platform Info

--- frontend/src/components/Session/SessionInfo/SessionPlatformInfo.tsx
+++ frontend/src/components/Session/SessionInfo/SessionPlatformInfo.tsx
@@ -1,4 +1,6 @@
-import { Flex, Icon } from '@chakra-ui/react';
+import { useMemo, forwardRef } from 'react';
+import { Flex, Icon, Tooltip } from '@chakra-ui/react';
+
 import browserIconMap from 'utils/browserIconMap';
 import deviceIconMap from 'utils/deviceIconMap';
 import osIconMap from 'utils/osIconMap';
@@ -9,17 +11,39 @@ const InfoIcon = ({ as }: { as: any }) => <Icon as={as} w={10} h={10} color="gra
 
 const SessionPlatformInfo = () => {
 	const { data } = useSessionInfo();
+
+	const device = useMemo(
+		() => (data.session.platform_info.isMobile ? 'mobile' : 'desktop'),
+		[data?.session.platform_info]
+	);
+	const browser = useMemo(
+		() => data.session.platform_info.browser,
+		[data?.session.platform_info]
+	);
+	const platform = useMemo(
+		() => data.session.platform_info.platform,
+		[data?.session.platform_info]
+	);
+
 	return data ? (
 		<Flex justifyContent="flex-end" flex="1" gap="3">
-			<InfoIcon
-				as={browserIconMap[data.session.platform_info.browser] || browserIconMap.default}
-			/>
-			{!!osIconMap[data.session.platform_info.platform] && (
-				<InfoIcon as={osIconMap[data.session.platform_info.platform]} />
+			<Tooltip label={'Browser: ' + browser}>
+				<div>
+					<InfoIcon as={browserIconMap[browser] || browserIconMap.default} />
+				</div>
+			</Tooltip>
+			{!!osIconMap[platform] && (
+				<Tooltip label={'Platform: ' + platform}>
+					<div>
+						<InfoIcon as={osIconMap[platform]} />
+					</div>
+				</Tooltip>
 			)}
-			<InfoIcon
-				as={deviceIconMap[data.session.platform_info.isMobile ? 'mobile' : 'desktop']}
-			/>
+			<Tooltip label={'Device: ' + device}>
+				<div>
+					<InfoIcon as={deviceIconMap[device]} />
+				</div>
+			</Tooltip>
 		</Flex>
 	) : (
 		<></>
