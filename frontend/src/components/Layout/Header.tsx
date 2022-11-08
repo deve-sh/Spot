df92882b711cdbc3c800b747a354eaea1e6c5f6b
Devesh Kumar <devesh2027@gmail.com>
2022-11-08 17:39:36 +0530

    frontend: Hid Sign In Button in case User is Logged In

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -1,26 +1,34 @@
+import dynamic from 'next/dynamic';
 import { Box, Flex } from '@chakra-ui/react';
 
 import Container from './Container';
 import Logo from './Logo';
-import SignInWithGitHub from './SignInWithGitHub';
 
-const Header = () => (
-	<Box
-		borderBottomColor="gray.200"
-		borderBottomWidth="1.125px"
-		background="white"
-		color="black"
-		padding="2"
-	>
-		<Container display="flex" alignItems="center">
-			<Box flex="1.33">
-				<Logo />
-			</Box>
-			<Flex flex="1" justifyContent="flex-end">
-				<SignInWithGitHub />
-			</Flex>
-		</Container>
-	</Box>
-);
+import useUser from 'hooks/auth/useUser';
+
+const SignInWithGitHub = dynamic(() => import('./SignInWithGitHub'), { ssr: false });
+
+const Header = () => {
+	const user = useUser();
+
+	return (
+		<Box
+			borderBottomColor="gray.200"
+			borderBottomWidth="1.125px"
+			background="white"
+			color="black"
+			padding="3"
+		>
+			<Container display="flex" alignItems="center">
+				<Box flex="1.33">
+					<Logo />
+				</Box>
+				<Flex flex="1" justifyContent="flex-end">
+					{!user ? <SignInWithGitHub /> : ''}
+				</Flex>
+			</Container>
+		</Box>
+	);
+};
 
 export default Header;
