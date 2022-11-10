f62b1b4583cf837b7d0cd3fbf882950f5ac7d812
Devesh Kumar <devesh2027@gmail.com>
2022-11-10 20:41:17 +0530

    Web SDK: Added Trace Timing and endpoint to config

--- web-sdk/src/config.ts
+++ web-sdk/src/config.ts
@@ -2,8 +2,10 @@ const config = {
 	BACKEND_URL: 'http://localhost:5100',
 	MONITORING_DATA_ENDPOINT: 'web-monitoring',
 	LOG_DATA_ENDPOINT: 'web-logs',
+	LOG_TRACES_ENDPOINT: 'web-custom-traces',
 	MONITORING_INTERVAL: 4000,
-	LOG_SENDING_DEBOUNCE_TIME: 1000
+	LOG_SENDING_DEBOUNCE_TIME: 1000,
+	TRACES_SENDING_DEBOUNCE_TIME: 2000
 };
 
 export default config;
