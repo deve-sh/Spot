e058dd093717bc486044be53123242f2fc7a3ac3
Devesh Kumar <devesh2027@gmail.com>
2022-11-13 15:45:11 +0530

    Web SDK: Updated API Endpoint for SDK

--- web-sdk/src/config.ts
+++ web-sdk/src/config.ts
@@ -1,5 +1,8 @@
 const config = {
-	BACKEND_URL: 'http://localhost:5100',
+	BACKEND_URL:
+		process.env.NODE_ENV === 'production'
+			? 'https://spot-monitoring-api.vercel.app'
+			: 'http://localhost:5100',
 	MONITORING_DATA_ENDPOINT: 'web-monitoring',
 	LOG_DATA_ENDPOINT: 'web-logs',
 	LOG_TRACES_ENDPOINT: 'web-custom-traces',
