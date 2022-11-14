4859435a59a0c2b806acc08cde7fce5392990bf7
Devesh Kumar <devesh2027@gmail.com>
2022-11-14 20:51:19 +0530

    frontend: Small import removal

--- frontend/src/components/Homepage/Footer.tsx
+++ frontend/src/components/Homepage/Footer.tsx
@@ -1,5 +1,5 @@
 import { Flex, Icon, Text } from '@chakra-ui/react';
-import { FaCoffee, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
 	return (
