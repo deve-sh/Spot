86f47d41beab6c1ef19c56bf11f137247bdfaa47
Devesh Kumar <devesh2027@gmail.com>
2022-11-14 17:13:33 +0530

    frontend: Updated labels and fill for other usage actions

--- frontend/src/utils/getFillForUsageAction.ts
+++ frontend/src/utils/getFillForUsageAction.ts
@@ -5,6 +5,10 @@ const getFillForUsageAction = (action: string) => {
 	if (action === 'get_top_session_domains') return '#f2d492';
 	if (action === 'list_sessions') return '#f29559';
 	if (action === 'get_project_vitals') return '#0c1618';
+	if (action === 'get_session_network_calls') return '#d1ac00';
+	if (action === 'get_session_info') return '#f6be9a';
+	if (action === 'get_session_logs') return '#ff101f';
+	if (action === 'get_session_vitals') return '#2176ae';
 	return '#004643';
 };
 
--- frontend/src/utils/getLabelForUsageAction.ts
+++ frontend/src/utils/getLabelForUsageAction.ts
@@ -5,6 +5,10 @@ const getLabelForUsageAction = (action: string) => {
 	if (action === 'get_top_session_domains') return 'Get Project Session Domains';
 	if (action === 'list_sessions') return 'List Project Sessions';
 	if (action === 'get_project_vitals') return 'Get Project Vitals';
+	if (action === 'get_session_network_calls') return 'Get Session Network Calls';
+	if (action === 'get_session_info') return 'Get Session Info';
+	if (action === 'get_session_logs') return 'Get Session Logs';
+	if (action === 'get_session_vitals') return 'Get Session Vitals';
 	return 'Untagged';
 };
 
