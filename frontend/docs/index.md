e404dabaa168aa0bf152ecc38764c060d9f86bd3
Devesh Kumar <devesh2027@gmail.com>
2022-11-15 10:42:04 +0530

    frontend: Added controls for documentation navigation

--- frontend/docs/index.md
+++ frontend/docs/index.md
@@ -1,1 +1,14 @@
-# Spot - The Definitive Frontend Monitoring and Observability Solution
+# Spot
+
+Spot is an elegant solution for monitoring and observing real user data for your web applications.
+
+Its features include:
+
+-   Web Vitals Monitoring
+-   Network Calls Performance Monitoring
+-   Session Diagnostics
+-   Session Network Waterfall
+-   Monitoring of Custom Code
+-   More to Come
+
+It's easy to get started with.
--- frontend/src/components/Docs/Layout.tsx
+++ frontend/src/components/Docs/Layout.tsx
@@ -1,10 +1,53 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { useMemo } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { Box, Flex, Icon } from '@chakra-ui/react';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { MdArrowRightAlt, MdOutlineArrowLeft } from 'react-icons/md';
 
 import Container from 'components/Layout/Container';
 import DocsNavbar from './Navbar';
+import docsNavList from '../../../docs/navlist';
+
+const MarkdownStyling = css`
+	.docs-block {
+		line-height: 2;
+
+		p {
+			margin: 0.75rem 0;
+		}
+
+		h1 {
+			font-size: 1.75rem;
+			color: var(--chakra-colors-blackAlpha-800);
+			font-weight: 700;
+			line-height: 1.2;
+		}
+
+		h2 {
+			font-size: 1.5rem;
+			color: var(--chakra-colors-blackAlpha-800);
+			font-weight: 700;
+			line-height: 1.2;
+		}
+
+		h3 {
+			font-size: 1.25rem;
+			color: var(--chakra-colors-blackAlpha-800);
+			font-weight: 700;
+			line-height: 1.2;
+		}
+
+		ul {
+			margin-left: var(--chakra-space-4);
+		}
+	}
+`;
 
 const DocsContainer = styled(Container)`
+	${MarkdownStyling}
+
 	@media (max-width: 768px) {
 		.docs-layout-flex {
 			flex-flow: column;
@@ -13,13 +56,46 @@ const DocsContainer = styled(Container)`
 `;
 
 const Layout = ({ children }: { children: React.ReactNode | React.ReactNode[] }) => {
+	const { query: { docPage } = {} } = useRouter();
+
+	const currentPageIndex = useMemo(() => {
+		if (docPage) return docsNavList.findIndex((navItem) => navItem.url === `/${docPage}`);
+		return -1;
+	}, [docPage]);
+
+	const nextPage = useMemo(() => {
+		if (currentPageIndex !== -1) return docsNavList[currentPageIndex + 1];
+	}, [currentPageIndex]);
+
+	const previousPage = useMemo(() => {
+		if (currentPageIndex !== -1) return docsNavList[currentPageIndex - 1];
+	}, [currentPageIndex]);
+
 	return (
 		<>
 			<DocsContainer>
 				<Flex className="docs-layout-flex">
 					<DocsNavbar />
-					<Box padding="4" color="gray.500">
-						{children}
+					<Box className="docs-block" padding="4" color="gray.500">
+						<Box>{children}</Box>
+						<Flex width="100%">
+							<Flex flex="1" justifyContent="flex-end" alignItems="center">
+								{!!previousPage && (
+									<>
+										<Icon as={MdOutlineArrowLeft} height={6} width={6} mr="2" />
+										<Link href={previousPage.url}>{previousPage.title}</Link>
+									</>
+								)}
+							</Flex>
+							<Flex flex="1" justifyContent="flex-end" alignItems="center">
+								{!!nextPage && (
+									<>
+										<Link href={nextPage.url}>{nextPage.title}</Link>
+										<Icon as={MdArrowRightAlt} height={6} width={6} ml="2" />
+									</>
+								)}
+							</Flex>
+						</Flex>
 					</Box>
 				</Flex>
 			</DocsContainer>
