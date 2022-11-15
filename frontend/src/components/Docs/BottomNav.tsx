e1fecf63f94bf441aeac2c9937ced2c2a9c0bdef
Devesh Kumar <devesh2027@gmail.com>
2022-11-15 10:47:53 +0530

    frontend: Styling updated for Bottom Nav

--- frontend/src/components/Docs/BottomNav.tsx
+++ frontend/src/components/Docs/BottomNav.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import { useMemo } from 'react';
 import Link from 'next/link';
 import { Icon, Flex } from '@chakra-ui/react';
-import { MdArrowRightAlt, MdOutlineArrowLeft } from 'react-icons/md';
+import { BsArrowRight, BsArrowLeft } from 'react-icons/bs';
 
 import docsNavList from '../../../docs/navlist';
 
@@ -24,19 +24,19 @@ const DocsBottomNav = () => {
 
 	return (
 		<Flex width="100%">
-			<Flex flex="1" justifyContent="flex-end" alignItems="center">
+			<Flex flex="1" justifyContent="flex-start" alignItems="center">
 				{!!previousPage && (
 					<>
-						<Icon as={MdOutlineArrowLeft} height={6} width={6} mr="2" />
-						<Link href={previousPage.url}>{previousPage.title}</Link>
+						<Icon as={BsArrowLeft} height={6} width={6} mr="2" />
+						<Link href={`/docs/${previousPage.url}`}>{previousPage.title}</Link>
 					</>
 				)}
 			</Flex>
 			<Flex flex="1" justifyContent="flex-end" alignItems="center">
 				{!!nextPage && (
 					<>
-						<Link href={nextPage.url}>{nextPage.title}</Link>
-						<Icon as={MdArrowRightAlt} height={6} width={6} ml="2" />
+						<Link href={`/docs/${nextPage.url}`}>{nextPage.title}</Link>
+						<Icon as={BsArrowRight} height={6} width={6} ml="2" />
 					</>
 				)}
 			</Flex>
