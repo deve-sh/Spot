cb53a361373c29979d0c4d4f22d8083b6256f7ef
Devesh Kumar <devesh2027@gmail.com>
2022-11-15 10:24:48 +0530

    frontend: Added /docs redirec to /docs/index

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	swcMinify: true
+	swcMinify: true,
+	redirects: () => [{ source: '/docs', destination: '/docs/index', permanent: true }]
 };
 
 module.exports = nextConfig;
